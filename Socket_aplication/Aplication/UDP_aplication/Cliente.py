4387804f70a9d372fd47a9ad81d447056d2b88c5
Mayelle Carvalho <carvalhomayelle@gmail.com>
2019-05-26 23:03:51 -0300

    TESTE PARA RECEBIMENTO DE ARQUIVO

--- Socket_aplication/Aplication/UDP_aplication/Cliente.py
+++ Socket_aplication/Aplication/UDP_aplication/Cliente.py
@@ -1,20 +1,63 @@
+# import socket
+# from pip._vendor.distlib.compat import raw_input
+#
+# host_server = '127.0.0.1'
+# porta_server = 4850
+#
+# socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#
+# socket_cliente.connect((host_server, porta_server))
+# print('Conectado ao servidor')
+#
+# mensagem = raw_input('Digite uma msg: ')
+#
+# while mensagem != '27':
+#     socket_cliente.sendto(mensagem.encode(), ((host_server, porta_server)))
+#     mensagem_server = socket_cliente.recvfrom(2048)
+#     print(mensagem_server)
+#     mensagem = raw_input('Digite uma msg: ')
+#
+# socket_cliente.close()
+
+
+##teste:
+
+import math
+import os
 import socket
+import time
+
 from pip._vendor.distlib.compat import raw_input
 
 host_server = '127.0.0.1'
-porta_server = 4850
+porta_server = 3020
 
 socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 socket_cliente.connect((host_server, porta_server))
 print('Conectado ao servidor')
 
-mensagem = raw_input('Digite uma msg: ')
+nome_arquivo = input('Informe um arquivo: ')
+arquivo = open(nome_arquivo,'rb')
 
-while mensagem != '27':
-    socket_cliente.sendto(mensagem.encode(), ((host_server, porta_server)))
-    mensagem_server = socket_cliente.recvfrom(2048)
-    print(mensagem_server)
-    mensagem = raw_input('Digite uma msg: ')
+conteudo = arquivo.read(1024)
 
+qtd_envio = math.ceil(os.path.getsize(nome_arquivo) / 1024)
+
+# socket_cliente.sendto('Arquivo sendo enviado, informações: %d  partes' + str(qtd_envio).encode('utf-8'), ((host_server, porta_server)))
+socket_cliente.sendto(str(qtd_envio).encode('utf-8'), ((host_server, porta_server)))
+
+cont = 1
+tempo_espera = 0.000
+while conteudo:
+    time.sleep(tempo_espera)
+    tempo_espera +=10
+
+    socket_cliente.sendto(conteudo, ((host_server, porta_server)))
+    print('Enviado parte %d de %d partes: ' % (cont, qtd_envio))
+    # envio de próxima parte de byte
+    conteudo = arquivo.read(1024)
+    cont += 1
+
+arquivo.close()
 socket_cliente.close()
--- Socket_aplication/Aplication/UDP_aplication/Server.py
+++ Socket_aplication/Aplication/UDP_aplication/Server.py
@@ -1,25 +1,85 @@
+# import socket
+# from pip._vendor.distlib.compat import raw_input
+#
+# host = ''
+# porta = 4801
+#
+# socket_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# print('Socket criado!')
+#
+# socket_server.bind((host,porta))
+# print('Socket vinculado a porta!')
+#
+# print('\nSocket aguardando requisição...')
+#
+# while True:
+#     #recebe msg do cliente:
+#     msg_cliente, cliente = socket_server.recvfrom(2048)
+#     print(cliente, msg_cliente.decode())
+#
+#     #envio de resposta ao cliente:
+#     mensagem = raw_input('Digite uma resposta: ')
+#     socket_server.sendto(mensagem.encode(),cliente)
+#
+#
+# socket_server.close()
+
+
+## teste:
+
+
 import socket
+import time
+
 from pip._vendor.distlib.compat import raw_input
 
 host = ''
-porta = 4850
+porta = 3020
 
+cont = 1
 socket_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print('Socket criado!')
 
 socket_server.bind((host,porta))
-print('Socket vinculado a porta!')
-
-print('\nSocket aguardando requisição...')
+print('Socket vinculado a porta!\nSocket aguardando conexão de cliente...')
 
 while True:
-    #recebe msg do cliente:
-    msg_cliente, cliente = socket_server.recvfrom(2048)
-    print(cliente, msg_cliente.decode())
+    #recebe informação do arquivo do cliente:
+    conteudo_cliente, endereco_cliente = socket_server.recvfrom(1024)
+    print('Conectado à Cliente: ', endereco_cliente)
 
-    #envio de resposta ao cliente:
-    mensagem = raw_input('Digite uma resposta: ')
-    socket_server.sendto(mensagem.encode(),cliente)
+    qtd_envio = 0
+    qtd_recebido = 0
+    #controla a quantidade de partes do arquivo enviado:
+    if conteudo_cliente != None:
+        qtd_envio = int(conteudo_cliente)
+        print('Arquivo possui %d  partes' % (qtd_envio))
+    else:
+        print('Quantidade de partes não foi enviado ou enviado com erro')
+        break
 
+    # nome_arquivo = 'arquivo00'+str(cont)+'.txt'
+    arquivo = open('arquivo_00'+str(cont)+'.txt', 'wb')
+    while True:
+        #inicio de recebimento do conteudo do arquivo
+        inicio = time.time() * 1000
+        conteudo_cliente, endereco_cliente = socket_server.recvfrom(1024)
+        fim = time.time() * 1000
+        latencia = fim - inicio
+        velocidade = 1000 / latencia * 1024
+
+        if conteudo_cliente != None:
+            #escreve o conteudo no arquivo criado:
+            arquivo.write(conteudo_cliente)
+            qtd_recebido += 1
+            print('Recebido %d de %d partes' %(qtd_recebido, qtd_envio))
+            print('Latência: %.3f' +latencia,'\n Velocidade: %.2f KB/s ' % (velocidade/1000))
+
+        if qtd_recebido == qtd_envio :
+            arquivo.close()
+            cont += 1
+            print('Arquivo recebido com sucesso!')
+            break
 
 socket_server.close()
+
